0c4200fdabc7ca690105d304ec52f87f1166a7cd
Priyanshu Dasgupta <priyanshu.dasgupta8@gmail.com>
2020-10-10 14:29:29 -0700

    everything

---
+++ src/Speech.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
+
+const Speech = () => {
+    const [value, setValue] = React.useState('');
+    const { speak } = useSpeechSynthesis();
+    return (
+        <div className="speech">
+            <div className="group">
+                <h3>Text to Speech</h3>
+            </div>
+            <div className="group">
+                <textarea rows="10" value={value} onChange={e => setValue(e.target.value)}></textarea>
+            </div>
+            <div className="group">
+                <button onClick={() => speak({text: value})}>Speak</button>
+            </div>
+        </div>
+    );
+}
+
+export default Speech;
